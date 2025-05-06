22514bb12a00fbd03997976e8b7559b65d1d84c9
shahwar-coder <naqvishahwar120@gmail.com>
2025-05-07 00:01:50 +0530

    int changes

--- int.py
+++ int.py
@@ -1,15 +1,41 @@
-user_input = input()
+'''
+1. Check Alternating Boolean List
+Problem:
+Given a list of boolean values (True and False), determine if the list alternates between True and False without any consecutive duplicates.
 
-def str_to_int_possible(user_input):
-    for character in user_input:
-        ord_character = ord(character)
-        if ord_character not in range(48,58):
+Input:
+[True, False, True, False, True]
+
+Output:
+True
+
+Input:
+[True, True, False]
+
+Output:
+False
+'''
+
+def is_alternate(list_of_bools):
+    for index in range(len(list_of_bools)-1):
+        print(f"Taking {list_of_bools[index]} and {list_of_bools[index+1]} = {list_of_bools[index] + list_of_bools[index+1]}")
+        if list_of_bools[index] + list_of_bools[index+1] != 1:
             return False
     return True
 
-is_possible = str_to_int_possible(user_input)
+def take_correct_bools_instead_of_strings(list_of_str_bools):
+    list_of_actual_bools=[]
+    for i in list_of_str_bools:
+        if i == "True":
+            list_of_actual_bools.append(True)
+        elif i == "False":
+            list_of_actual_bools.append(False)
+    return list_of_actual_bools
 
-if is_possible:
-    print(int(user_input))
-else:
-    print("Invalid input, cannot be converted to integer")
+try:
+    streak_of_bools = input()
+    list_of_str_bools = streak_of_bools.split(" ")
+    list_of_bools = take_correct_bools_instead_of_strings(list_of_str_bools)
+    print(is_alternate(list_of_bools))
+except ValueError:
+    print("Invalid Input!")
