3360bf5ca69e73d80228ab609382bbe2cb04b296
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-05-13 21:50:13 +0530

    Create int_1_basics.py

---
+++ int_1_basics.py
@@ -0,0 +1,8 @@
+'''
+1. built in class
+2. immutable type
+3. can be very large, is not limited to 32 bit or 64 bit as in other languages
+'''
+
+x = 12222233333344444555555666666777777888888999999000000000
+print(type(x))
