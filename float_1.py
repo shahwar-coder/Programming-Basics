2f293aaf5597229de98abdc1757e4a2e58b7e56d
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-05-14 00:04:44 +0530

    Delete float_1.py

--- float_1.py
+++
@@ -1,7 +0,0 @@
-# take float and print it, Value error added great value in catching something when input is not float.
-
-try:
-    float_number = float(input("Enter a float number : "))
-    print(f"Float Number : {float_number}")
-except ValueError:
-    print("What you have input is not floating number!")
