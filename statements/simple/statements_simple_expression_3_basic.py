5bf4d6d0d3d273010f6bf9af0cb30849f2480bb3
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-06-07 13:46:10 +0530

    Create statements_simple_expression_3_basic.py

---
+++ statements/simple/statements_simple_expression_3_basic.py
@@ -0,0 +1,16 @@
+'''
+✅ Question 2: Useless Evaluation
+
+x = 10
+y = 20
+x + y
+
+Question:
+What happens when this code is run in a .py file (not in the Python shell)?
+Will it print or return anything? Why or why not?
+'''
+
+# This is a classic example for "Useless Evaluation"
+# Nothing is printed at all
+# No use of evaluation
+# This is also an statement expression but 'Useless One'
