8b3ab18885e0952f2d1535d1a88230afea99e604
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-06-11 14:20:04 +0530

    Update statement_simple_assert_2_basic.py

--- statements/simple/statement_simple_assert_2_basic.py
+++ statements/simple/statement_simple_assert_2_basic.py
@@ -20,3 +20,11 @@ print("This line prints?")
 # Output: First "Assertion passed!" will be printed as x>5 is 10>5 (True)
 # Then : AssertionError will be thrown, as x is not less than 5.
 # And : "This line prints?" WON'T BE PRINTED.
+
+# Notes:
+'''
+- assert is used to catch logical errors early.
+- If the condition fails, AssertionError is raised.
+- Optional error messages help clarify the failure.
+- Execution stops immediately at the point of failure.
+'''
