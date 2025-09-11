1cc34789d95418326349e8c42ade20f1b1d7f455
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-11 11:08:43 +0530

    Create simple_vs_compond_statements.py

---
+++ statements/basics/simple_vs_compond_statements.py
@@ -0,0 +1,18 @@
+'''
+📌 Simple Statements
+• Smallest unit of instruction (one logical line)
+• Examples: pass, x = 5, return 3, break, continue
+• Multiple simple statements can share one line using ;
+'''
+pass
+
+'''
+📌 Compound Statements
+• Control or group other statements
+• Structure = header (ends with :) + suite (indented block)
+• Examples: if, for, while, try, with, def, class, match
+'''
+if x > 0:           # header
+    print("positive")   # suite
+else:
+    print("not positive")
