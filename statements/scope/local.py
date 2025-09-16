74941c1d804b046f6d880b73035e41d76cf069f4
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-16 09:33:21 +0530

    Create local.py

---
+++ statements/scope/local.py
@@ -0,0 +1,14 @@
+'''
+Q5. How does local scope work in Python? Show with an example.
+Ans: A variable created inside a function is local to that function by default.  
+It cannot be accessed outside the function.
+
+Example:
+'''
+
+def f():
+    x = 42   # local to f
+    print("Inside f:", x)
+
+f()
+# print(x)  # ❌ Error → NameError: x is not defined (outside local scope)
