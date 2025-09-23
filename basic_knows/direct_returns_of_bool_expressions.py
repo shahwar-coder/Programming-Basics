27ccaa79d84eafbd08e387243fdb217b7a8856ed
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-23 21:44:26 +0530

    Create direct_returns_of_bool_expressions.py

---
+++ basic_knows/direct_returns_of_bool_expressions.py
@@ -0,0 +1,8 @@
+def is_adult(age: int) -> bool:
+    return age >= 18  # Direct boolean return
+
+'''
+- Cleaner and shorter than using if/else to return True/False.
+- Improves readability and makes intent obvious at first glance.
+- Idiomatic "Pythonic" style → preferred in professional codebases.
+'''
