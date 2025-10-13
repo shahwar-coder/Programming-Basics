d0100fe556c6d00fb9f3cf475f2d161ff644058e
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-13 22:48:04 +0530

    Update itertools_permutaions.py

--- basic_knows/itertools_permutaions.py
+++ basic_knows/itertools_permutaions.py
@@ -1,1 +1,18 @@
-'''
+from itertools import permutations
+
+data = ['A', 'B', 'C']
+result = permutations(data, 2)
+
+for p in result:
+    print(p)
+
+
+# OUTPUT:
+# ('A', 'B')
+# ('A', 'C')
+# ('B', 'A')
+# ('B', 'C')
+# ('C', 'A')
+# ('C', 'B')
+
+# REPETITIONS ALLOWED (unlike combinations)
