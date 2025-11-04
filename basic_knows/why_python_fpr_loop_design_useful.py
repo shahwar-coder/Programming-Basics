2ed4b2a33069b6a7f191d4c5b86ab7ae62bd3dbf
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-04 14:28:18 +0530

    Create why_python_fpr_loop_design_useful.py

---
+++ basic_knows/why_python_fpr_loop_design_useful.py
@@ -0,0 +1,6 @@
+'''
+Q. What’s the main advantage of Python’s for loop design?
+Ans:
+It’s *iterator-based* — meaning it works for any iterable,  
+doesn’t rely on manual indexing, and automatically handles iteration end.
+'''
