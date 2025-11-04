2da58f08edc7b4a1e7185728a713e011c54774d4
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-04 16:01:41 +0530

    Update zip_:_3_main_uses.py

--- basic_knows/zip_:_3_main_uses.py
+++ basic_knows/zip_:_3_main_uses.py
@@ -1,5 +1,5 @@
 '''
-Q5. What are some common uses of zip()?
+Q. What are some common uses of zip()?
 Ans:
 - Pairing related data (like names and scores)  
 - Looping through multiple lists together  
@@ -12,6 +12,7 @@ scores = [85, 90, 78]
 
 paired = list(zip(names, scores))
 print(paired)
+# [('Alice', 85), ('Bob', 90), ('Charlie', 78)]
 
 
 # Example 2 — Looping together
@@ -20,6 +21,9 @@ marks = [95, 88, 76]
 
 for subject, mark in zip(subjects, marks):
     print(f"{subject}: {mark}")
+# Math: 95
+# Science: 88
+# English: 76
 
 # ===================================================
 
@@ -39,4 +43,6 @@ names, scores = zip(*pairs)
 
 print(names)
 print(scores)
+# ('Alice', 'Bob', 'Charlie')
+# (85, 90, 78)
 
