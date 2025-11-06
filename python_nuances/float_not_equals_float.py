ec5caefeb33d799249964bfaec68219db5349ceb
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-06 08:37:11 +0530

    Update float_not_equals_float.py

--- python_nuances/float_not_equals_float.py
+++ python_nuances/float_not_equals_float.py
@@ -8,3 +8,16 @@ accounting for tiny floating-point errors that occur during calculations.
 import math
 print(math.isclose(0.1 + 0.2, 0.3))  # True
 print((0.1 + 0.2) == 0.3)            # False (due to rounding)
+
+
+'''
+Q. Why is using == unsafe for float comparison?
+Ans:
+Because floating-point arithmetic introduces microscopic errors —  
+so two logically equal results may differ by 1e-16 or less.
+'''
+# Example
+x = 0.3
+y = 0.1 + 0.2
+print(x == y)         # False
+print(math.isclose(x, y))  # True
