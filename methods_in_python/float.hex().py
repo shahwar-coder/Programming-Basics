f7955f37ed2f5e4687678c7b6e3d452ffa405f29
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-06 17:02:44 +0530

    Update float.hex().py

--- methods_in_python/float.hex().py
+++ methods_in_python/float.hex().py
@@ -9,3 +9,8 @@ This format is reversible using `float.fromhex()`.
 print((0.75).hex())     # '0x1.8p-1'
 print((1.0).hex())      # '0x1.0000000000000p+0'
 print((3.5).hex())      # '0x1.c000000000000p+1'
+
+# Example (same output)
+print(float.hex(0.75))     # '0x1.8p-1'
+print(float.hex(1.0))      # '0x1.0000000000000p+0'
+print(float.hex(3.5))      # '0x1.c000000000000p+1'
