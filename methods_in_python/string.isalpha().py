a124fe35789c1973a7baca1e7020fc4b865821fc
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 12:07:46 +0530

    Create string.isalpha().py

---
+++ methods_in_python/string.isalpha().py
@@ -0,0 +1,10 @@
+'''
+Q. What does isalpha() do?
+Ans:
+It returns **True** if all characters in the string are alphabetic (A–Z, a–z).  
+Returns False if it contains spaces, digits, or symbols.
+'''
+# Example
+print("Python".isalpha())   # True
+print("Python3".isalpha())  # False
+print("Hello!".isalpha())   # False
