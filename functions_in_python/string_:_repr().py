825937d58881016c893057477829f91c561a5b8e
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 17:34:05 +0530

    Update string_:_repr().py

--- functions_in_python/string_:_repr().py
+++ functions_in_python/string_:_repr().py
@@ -1,5 +1,5 @@
 '''
-Q. What does repr(x) do?
+Q1. What does repr(x) do?
 Ans:
 It returns a **developer-friendly** string form of an object —  
 includes quotes and escape characters for clarity.  
@@ -9,3 +9,17 @@ Often used for debugging or logging.
 text = "Hello\nWorld"
 print(str(text))   # Prints on two lines
 print(repr(text))  # 'Hello\nWorld'
+
+
+
+'''
+Q2. How are str() and repr() different in intent?
+Ans:
+- `str()` → human-readable (user-friendly)  
+- `repr()` → machine-readable (for developers, debugging)
+'''
+# Example
+import datetime
+d = datetime.date(2025, 11, 7)
+print(str(d))   # '2025-11-07' (friendly)
+print(repr(d))  # 'datetime.date(2025, 11, 7)' (precise)
