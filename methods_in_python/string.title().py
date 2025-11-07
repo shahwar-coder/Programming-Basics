d6a1a6c36cc20e6b6f29ce6050e1aaa9ca377ec9
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 11:47:00 +0530

    Create string.title().py

---
+++ methods_in_python/string.title().py
@@ -0,0 +1,9 @@
+'''
+Q. What does title() do?
+Ans:
+It capitalizes the **first letter of each word**,  
+making it ideal for names or titles.
+'''
+# Example
+print("welcome to python".title())   # "Welcome To Python"
+print("the great gatsby".title())    # "The Great Gatsby"
