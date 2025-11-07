03494dacb3e273f282ed99ec3226ab3cef762726
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 17:28:55 +0530

    Create string_:_chr().py

---
+++ functions_in_python/string_:_chr().py
@@ -0,0 +1,9 @@
+'''
+Q. What does chr(code) do?
+Ans:
+It performs the **inverse of ord()** —  
+returns the **character** corresponding to a Unicode code point.
+'''
+# Example
+print(chr(65))    # 'A'
+print(chr(8364))  # '€'
