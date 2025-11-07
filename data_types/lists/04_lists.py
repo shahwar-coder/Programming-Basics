812c87374201c4eb42450418cbfc2fa8ed800f3c
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 19:29:46 +0530

    Update 04_lists.py

--- data_types/lists/04_lists.py
+++ data_types/lists/04_lists.py
@@ -23,3 +23,24 @@ This creates a new list without modifying the original.
 nums = [1, 2]
 print(nums * 3)    # [1, 2, 1, 2, 1, 2]
 print(['Hi'] * 4)  # ['Hi', 'Hi', 'Hi', 'Hi']
+
+
+
+'''
+Q6. What is the difference between sort() and sorted()?
+Ans:
+- `sort()` → modifies the list **in-place**, returns None.  
+- `sorted(iterable)` → returns a **new sorted list**, leaving the original unchanged.
+'''
+
+# Example
+nums = [5, 3, 1, 4, 2]
+
+# Using sorted() → returns a new list
+new_list = sorted(nums)
+print("Original list:", nums)       # [5, 3, 1, 4, 2]
+print("Sorted copy: ", new_list)    # [1, 2, 3, 4, 5]
+
+# Using sort() → changes the same list
+nums.sort()
+print("After sort():", nums)        # [1, 2, 3, 4, 5]
