e3e6f58b7451e8642ab1ef7ea4cc3c7082c517ed
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 11:48:09 +0530

    Create string.capitalize().py

---
+++ methods_in_python/string.capitalize().py
@@ -0,0 +1,8 @@
+'''
+Q. What does capitalize() do?
+Ans:
+It capitalizes **only the first character** of the string  
+and converts the rest to lowercase.
+'''
+# Example
+print("hELLO wORLD".capitalize())   # "Hello world"
