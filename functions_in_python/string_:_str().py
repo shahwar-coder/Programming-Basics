521500e76b8ee5c3510a5583e1ac90e54b0345ed
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 17:30:20 +0530

    Create string_:_str().py

---
+++ functions_in_python/string_:_str().py
@@ -0,0 +1,10 @@
+'''
+Q. What does str(x) do?
+Ans:
+It converts a value into its **string representation**,  
+making it suitable for display or concatenation.
+'''
+# Example
+num = 123
+print(str(num))        # '123'
+print(type(str(num)))  # <class 'str'>
