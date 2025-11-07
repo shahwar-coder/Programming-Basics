e5beb14b9935595e352333eb5c64f3a27bc9fbda
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-07 11:49:47 +0530

    Update string.capitalize().py

--- methods_in_python/string.capitalize().py
+++ methods_in_python/string.capitalize().py
@@ -6,3 +6,16 @@ and converts the rest to lowercase.
 '''
 # Example
 print("hELLO wORLD".capitalize())   # "Hello world"
+
+
+
+'''
+Q. What’s the difference between title() and capitalize()?
+Ans:
+- `title()` → Capitalizes *every* word’s first letter.  
+- `capitalize()` → Only the *first word*; rest lowercase.=
+'''
+# Example
+s = "welcome to python"
+print(s.title())      # "Welcome To Python"
+print(s.capitalize()) # "Welcome to python"
