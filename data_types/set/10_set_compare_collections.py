8c6c893e1693d47609d9bcb3149423803244cd59
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-12-24 12:26:42 +0530

    Update 10_set_compare_collections.py

--- data_types/set/10_set_compare_collections.py
+++ data_types/set/10_set_compare_collections.py
@@ -8,3 +8,11 @@ b = {3, 4}
 a & b   # {3}   → common
 a | b   # {1, 2, 3, 4} → all
 a - b   # {1, 2} → only in a
+
+'''
+Q. What is a real-life use case of set comparison?
+Ans. Finding common or missing items between two groups.
+'''
+# Example
+# Students in Math & Science → intersection
+# Students only in Math → difference
