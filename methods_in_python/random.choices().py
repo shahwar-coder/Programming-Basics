69f7b335fd8423bb52bf1cf30d738da481a27cba
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-12-27 14:36:09 +0530

    Update random.choices().py

--- methods_in_python/random.choices().py
+++ methods_in_python/random.choices().py
@@ -4,3 +4,7 @@ import random
 print("choices([1, 2, 3], k=2):", random.choices([1, 2, 3], k=2))
 # → Output: list of elements (duplicates allowed)
 # Returns k random elements with repetition
+
+'''
+random.choices(population, weights=None, *, cum_weights=None, k=1)
+'''
