5792f40914bb8e8bef6f009063502b8c96f81127
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2026-01-28 18:11:59 +0530

    Create 08__hash__.py

---
+++ OOPs/Dunder_Methods/08__hash__.py
@@ -0,0 +1,10 @@
+'''
+__hash__ : a fast “number fingerprint” for an object.
+You don't keep searching, the `number fingerprint` leads you.
+
+__hash__ is required in Python because Python has hash-based-collections:
+like-> dicts and sets
+
+__hash__ tells Python WHERE to look
+__eq__ tells Python IS it really the same
+'''
